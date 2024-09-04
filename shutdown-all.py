f9f74a03b8d524cb31ded438c1c8a6c95d731bbc
sky20041129 <46390548+sky20041129@users.noreply.github.com>
2024-09-04 17:59:17 +0800

    Delete shutdown-all.py

--- shutdown-all.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import sys
-
-try:
-    subprocess.check_call(['python', 'cmdall.py', 'poweroff'])
-except subprocess.CalledProcessError as e:
-    error_message = "Shutdown command failed with error: {}".format(e)
-    print(error_message)
-    sys.exit(1)  # Exit on failure
